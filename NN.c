9a34a76c9ed6210f507292929baeab3d12d64ad2
Bhavya010507 <srivastavabhavya77@gmail.com>
2025-08-30 15:38:56 +0530

    Add files via upload

---
+++ NN.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+ {
+    int n, sum = 0;
+    printf("Enter a positive integer:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("The sum of first n natural numbers are:%d\n",sum);
+    return 0;
+}
