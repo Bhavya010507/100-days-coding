e772a2a6620bd3d31c27979b7e577980096ce739
Bhavya010507 <srivastavabhavya77@gmail.com>
2025-09-28 20:36:55 +0530

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main() {
+    char operator;
+    double num1, num2, result;
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c", &operator);
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+   switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        case '%':
+            // Modulus operator works only with integers in C
+            if (num2 != 0) {
+                // Cast doubles to integers for modulus operation
+                result = (int)num1 % (int)num2;
+                printf("%d %% %d = %.2lf\n", (int)num1, (int)num2, result);
+            } else {
+                printf("Error: Modulus by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("Error: Invalid operator entered.\n");
+    }
+
+    return 0;
+}
