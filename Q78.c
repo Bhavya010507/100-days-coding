a271d0e9dcb2bb528f5b9845aa23aafdb46ecf2f
Bhavya010507 <srivastavabhavya77@gmail.com>
2025-11-30 23:52:03 +0530

    Implement matrix diagonal sum calculation

---
+++ Q78.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, sum = 0;
+
+    printf("Enter size of square matrix (n x n): ");
+    scanf("%d", &n);
+
+    int mat[n][n];
+
+    // Read matrix
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    // Calculate sum of main diagonal
+    for (i = 0; i < n; i++) {
+        sum += mat[i][i];
+    }
+
+    // Print result
+    printf("Sum of main diagonal elements = %d\n", sum);
+
+    return 0;
+}
